efbeab8ed99deee89e1f8d508c9654568af2f9a9
David Crosby <dave@dafyddcrosby.com>
2020-03-15 17:21:06 -0600

    Move logic to Board impl

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,51 @@
 extern crate libusb;
 
+use std::thread::sleep;
 use std::time::Duration;
-
 // DC board USB ID
 const VENDORID: u16 = 0x1d34;
 const PRODUCTID: u16 = 0x0013;
 
-struct Board {
+#[derive(Copy, Clone)]
+struct Row([bool; 21]);
+
+struct Board<'a> {
+    device_handle: &'a libusb::DeviceHandle<'a>,
     brightness: u8,
-    leds: [[bool; 21]; 7],
+    leds: [Row; 7], // lit if true
 }
 
-impl Board {
+impl<'a> Board<'a> {
+    fn new(dh: &'a libusb::DeviceHandle<'a>) -> Board<'a> {
+        Board {
+            device_handle: dh,
+            brightness: 0,
+            leds: [Row([false; 21]); 7],
+        }
+    }
     fn draw(&self) {
-        for row in &self.leds {}
+        let packets = [
+            USBPacket::new(0, 0, &self.leds[0], &self.leds[1]),
+            USBPacket::new(0, 2, &self.leds[2], &self.leds[3]),
+            USBPacket::new(0, 4, &self.leds[4], &self.leds[5]),
+            USBPacket::new(0, 6, &self.leds[6], &self.leds[6]), // No row 7
+        ];
+
+        for packet in packets.iter() {
+            Board::<'a>::write_packet(&self, &packet.make_packet());
+        }
+    }
+
+    fn clear(&mut self) {
+        for i in 0..7 {
+            self.leds[i] = Row([false; 21]);
+        }
+    }
+
+    fn write_packet(&self, packet: &[u8]) {
+        self.device_handle
+            .write_control(0x21, 0x09, 0x0000, 0x0000, &packet, Duration::new(5, 0))
+            .unwrap();
     }
 }
 struct USBPacket {
@@ -24,6 +56,25 @@ struct USBPacket {
 }
 
 impl USBPacket {
+    fn new(brightness: u8, row: u8, data1: &Row, data2: &Row) -> USBPacket {
+        // TODO - actually proces data1 and data2
+        //for row in self.leds.iter() {
+        //    let mut byte: u8 = 0;
+        //    for (index, bit) in row.iter().enumerate() {
+        //        println!("{} {}", index % 8, bit);
+        //        let mask: u8 = 1 << (index % 8);
+        //        if *bit {
+        //            byte |= mask;
+        //        }
+        //    }
+        //}
+        USBPacket {
+            brightness: brightness,
+            row: row,
+            data1: [0xff, 0xfb, 0xbf],
+            data2: [0xff, 0xfb, 0xbf],
+        }
+    }
     fn make_packet(&self) -> [u8; 8] {
         // TODO There has to be a simpler way...
         [
@@ -43,12 +94,6 @@ fn is_dc_board(vendor: u16, product: u16) -> bool {
     vendor == VENDORID && product == PRODUCTID
 }
 
-// This would be better handled in the Board struct...
-fn write_packet(dh: &libusb::DeviceHandle, packet: &[u8]) {
-    dh.write_control(0x21, 0x09, 0x0000, 0x0000, &packet, Duration::new(5, 0))
-        .unwrap();
-}
-
 fn main() {
     let context = libusb::Context::new().unwrap();
 
@@ -59,17 +104,11 @@ fn main() {
             println!("Found a DC board");
             let handle = device.open().unwrap();
 
+            let mut b = Board::new(&handle);
             loop {
-                // TODO do something more interesting than writing the diamond test pattern ;-)
-                let diamond_test_packets = [
-                    [0x00, 0x00, 0xff, 0xfe, 0xff, 0xff, 0xfd, 0x7f],
-                    [0x00, 0x02, 0xff, 0xfb, 0xbf, 0xff, 0xf7, 0xdf],
-                    [0x00, 0x04, 0xff, 0xfb, 0xbf, 0xff, 0xfd, 0x7f],
-                    [0x00, 0x06, 0xff, 0xfe, 0xff, 0x00, 0x00, 0x00],
-                ];
-                for packet in diamond_test_packets.iter() {
-                    write_packet(&handle, packet);
-                }
+                b.draw();
+                std::thread::sleep(Duration::from_secs(1));
+                b.clear();
             }
         }
     }
